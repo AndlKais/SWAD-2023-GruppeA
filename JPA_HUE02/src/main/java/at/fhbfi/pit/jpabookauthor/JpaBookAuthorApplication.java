5ed0b2f8d9382ad2fc2bb3f8e0c4e832bccac6d3
Andreas <fhb211114@fh-vie.ac.at>
2023-10-23 19:28:18 +0200

    Sprint 2 Starter Project

---
+++ JPA_HUE02/src/main/java/at/fhbfi/pit/jpabookauthor/JpaBookAuthorApplication.java
@@ -0,0 +1,13 @@
+package at.fhbfi.pit.jpabookauthor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaBookAuthorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaBookAuthorApplication.class, args);
+	}
+
+}
---
+++ JPA_HUE02/src/test/java/at/fhbfi/pit/jpabookauthor/JpademoApplicationTests.java
@@ -0,0 +1,13 @@
+package at.fhbfi.pit.jpabookauthor;
+
+import at.fhbfi.pit.jpabookauthor.persistence.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+class JpademoApplicationTests {
+}
