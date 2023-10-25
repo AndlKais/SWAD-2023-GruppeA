a586bac54b4de5f68f1d8535c52d1fe6c66ce28a
Matthias Frankowski <fhb211108@fh-vie.ac.at>
2023-10-25 19:40:49 +0200

    Changes by jenni

--- JPA_HUE02/src/test/java/at/fhbfi/pit/jpabookauthor/JpademoApplicationTests.java
+++ JPA_HUE02/src/test/java/at/fhbfi/pit/jpabookauthor/JpademoApplicationTests.java
@@ -6,8 +6,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
 
 @SpringBootTest
 class JpademoApplicationTests {
+
+    @Autowired
+    private AuthorRepository authorRepository;
+
+    /*@Test
+    void testAuthorPersistence() {
+    List<AuthorEntity> authors = new ArrayList<>();
+        authors.add(AuthorEntity.builder()
+                .name("Matt")
+                .age(32)
+                .birthday(new Date(15,9,1989))
+                .dead(true)
+                .writtenBooks(4)
+                .build());
+
+        authorRepository.saveAll(authors);
+        authorRepository.findAll().forEach(System.out::println);
+
+    } */
+
 }
+
